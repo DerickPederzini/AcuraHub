2c09f219c03931e1fc364a6aed3723c71d2113e0
NicolasVPagliari <nicolasvpagliari@gmail.com>
2025-08-28 16:54:30 -0300

    add: create_page

--- forum_front/lib/constants/app_colors.dart
+++ forum_front/lib/constants/app_colors.dart
@@ -4,5 +4,6 @@ class AppColors {
 
   static const blue_eurofarma = Color.fromARGB(255, 0, 205, 240);
   static const grey_70 = Colors.white70;
+  static const text_create_page = Color.fromRGBO(170, 170, 170, 1);
 
 }
