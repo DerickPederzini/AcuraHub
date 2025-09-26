ff2cc33a18e2a9bddf697e64f49d5cb6f65ef6c7
DerickPederzini <derickpps@gmail.com>
2025-09-26 18:43:39 -0300

    teste drive de noticia

--- forum_front/lib/components/feed/feed_card.dart
+++ forum_front/lib/components/feed/feed_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:forum_front/constants/app_font.dart';
 import 'package:forum_front/constants/app_styles.dart';
+import 'package:forum_front/models/news.dart';
 
 class FeedCard extends StatefulWidget {
-  const FeedCard({super.key});
+  final News news;
+  const FeedCard({super.key, required this.news});
 
   @override
   State<FeedCard> createState() => _FeedCardState();
@@ -24,115 +26,34 @@ class _FeedCardState extends State<FeedCard> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    spacing: 8,
-                    children: [
-                      CircleAvatar(radius: 12,),
-                      Text("pdzn_mvp", style: TextStyle(fontSize: 12)),
-                      Text("12h", style: TextStyle(fontSize: 12),),
-                      Text("°", style: TextStyle(fontSize: 12),),
-                      Text("128 Views", style: TextStyle(fontSize: 12),),
-                    ],
-                  ),
-                  OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(minimumSize: Size(5, 30)),
-                    child: Text("Participar", style: TextStyle(fontSize: 12)),
-                  ),
-
-                ],
-              ),
-              SizedBox(height: 4,),
-              Text("Titulo do post", style: TextStyle(fontSize: 18),),
-              Text(maxLines: 3, overflow: TextOverflow.ellipsis, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium Tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia inteiro nunc posuere. Ut hendrerit sempre vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",)
-        
-            ],
-            ),
-            // ListTile(
-            //   leading: Column(children: [CircleAvatar(radius: 18,)]),
-            //   title: Row(
-            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //     children: [
-            //       Text("Title"),
-            //       OutlinedButton(
-            //         onPressed: () {},
-            //         style: OutlinedButton.styleFrom(minimumSize: Size(5, 30)),
-            //         child: Text("Participar", style: TextStyle(fontSize: 12)),
-            //       ),
-            //     ],
-            //   ),
-            //   subtitle: Text(
-            //     maxLines: 2,
-            //     overflow: TextOverflow.ellipsis,
-            //     "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium Tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia inteiro nunc posuere. Ut hendrerit sempre vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-            //   ),
-            // ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    spacing: 8,
-                    children: [
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          side: BorderSide(color: Colors.grey.shade800, width: 1),
-                        ),
-                        onPressed: () {},
-                        child: Row(
-                          children: [
-                            Icon(Icons.heart_broken, size: 14),
-                            SizedBox(width: 2),
-                            Text("12k", style: AppStyles.font_14_sans),
-                          ],
-                        ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      spacing: 8,
+                      children: [
+                        CircleAvatar(radius: 12, child: Text("${widget.news.author[0]}"),),
+                        Text("${widget.news.author}", style: TextStyle(fontSize: 12)),
+                        Text("12h", style: TextStyle(fontSize: 12)),
+                      ],
+                    ),
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Colors.grey.shade800, width: 1),
                       ),
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          side: BorderSide(color: Colors.grey.shade800, width: 1),
-                        ),
-                        onPressed: () {},
-                        child: Row(
-                          children: [
-                            Icon(Icons.comment, size: 14),
-                            SizedBox(width: 2),
-                            Text("500", style: AppStyles.font_14_sans),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          side: BorderSide(color: Colors.grey.shade800, width: 1),
-                          shape: CircleBorder(),
-                        ),
-                        onPressed: () {},
-                        child: Row(children: [Icon(Icons.star, size: 14)]),
-                      ),
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          side: BorderSide(color: Colors.grey.shade800, width: 1),
-                        ),
-                        onPressed: () {},
-                        child: Row(
-                          children: [
-                            Icon(Icons.share, size: 14),
-                            SizedBox(width: 2),
-                            Text("1380", style: AppStyles.font_14_sans),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                      onPressed: () {},
+                      child: Icon(Icons.favorite, size: 14),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 4),
+                Text("${widget.news.titulo}", style: TextStyle(fontSize: 18)),
+                Text(
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                  "${widget.news.body}",
+                ),
+              ],
             ),
           ],
         ),
---
+++ forum_front/lib/models/news.dart
@@ -0,0 +1,11 @@
+
+
+class News {
+
+  String author;
+  String titulo;
+  String body;
+
+  News({required this.author, required this.titulo, required this.body});
+
+}
